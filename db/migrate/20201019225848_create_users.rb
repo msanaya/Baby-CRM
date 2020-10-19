a8575d4e78efc9a7cc0962ca11370136287dd49b
msanaya <50601483+msanaya@users.noreply.github.com>
2020-10-19 19:00:31 -0400

    Scaffold user

--- db/migrate/20201019225848_create_users.rb
+++ db/migrate/20201019225848_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.serial :user_id
-      t.string :name
+      t.string :user_name
 
       t.timestamps
     end
