14409efb6e0bfcef53a80401224a51b724dd60e3
msanaya <50601483+msanaya@users.noreply.github.com>
2020-10-20 11:49:57 -0400

    Created scaffold for salespeople

---
+++ db/migrate/20201020154745_create_salespeople.rb
@@ -0,0 +1,13 @@
+class CreateSalespeople < ActiveRecord::Migration[6.0]
+  def change
+    create_table :salespeople do |t|
+      t.serial :salesperson_id
+      t.string :salesperson_username
+      t.string :salesperson_name
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
