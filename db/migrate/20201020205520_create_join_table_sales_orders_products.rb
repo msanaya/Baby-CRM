d977a875a3aa20da2f2d63b4f134bc235461b75b
msanaya <50601483+msanaya@users.noreply.github.com>
2020-10-24 21:37:34 -0400

    Auth works

--- db/migrate/20201020205520_create_join_table_sales_orders_products.rb
+++ db/migrate/20201020205520_create_join_table_sales_orders_products.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableSalesOrdersProducts < ActiveRecord::Migration[6.0]
   def change
     create_join_table :sales_orders, :products do |t|
-      # t.index [:sales_order_id, :product_id]
-      # t.index [:product_id, :sales_order_id]
+      t.index [:sales_order_id, :product_id]
+      t.index [:product_id, :sales_order_id]
     end
   end
 end
